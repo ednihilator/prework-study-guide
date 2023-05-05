eb9af7a0c84ec710451d02c80bf7dd7146ea6058
ednihilator <boyofpopsicle@gmail.com>
2023-05-05 14:32:25 -0400

    t

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,12 @@ for (var x=0; x<topics.length; x++){
   console.log(topics[x]);
 
 }
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
 
-/*
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -18,4 +22,3 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-*/
